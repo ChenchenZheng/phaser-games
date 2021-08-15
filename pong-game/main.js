70ac256811446660c23b3dec044ef574c262257e
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-08-15 09:19:24 +0200

    ✨ Pong game two players done

--- pong-game/main.js
+++ pong-game/main.js
@@ -1,6 +1,7 @@
 var mainState = {
   preload: function(){
     game.load.image('paddle', 'paddle.png');
+    game.load.image('ball', 'ball.png');
   },
 
   create: function(){
@@ -8,15 +9,46 @@ var mainState = {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.world.enableBody = true;
 
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+
+    //player
     this.player = game.add.sprite(game.width/2, game.height-40, 'paddle');
     this.player.anchor.setTo(0.5);
     this.player.body.collideWorldBounds = true;
+    this.player.body.immovable = true;
 
     this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+    //player2
+    this.player2 = game.add.sprite(game.width/2, 40, 'paddle');
+    this.player2.anchor.setTo(0.5);
+    this.player2.body.collideWorldBounds = true;
+    this.player2.body.immovable = true;
+
+    this.leftP2 = game.input.keyboard.addKey(Phaser.Keyboard.Q);
+    this.rightP2 = game.input.keyboard.addKey(Phaser.Keyboard.D);
+
+    //ball
+    this.ball = game.add.sprite(game.width/2, game.height/2, 'ball');
+    this.ball.anchor.setTo(0.5);
+    this.ball.body.collideWorldBounds = true;
+    this.ball.body.velocity.x = 300;
+    this.ball.body.velocity.y = 300;
+    this.ball.body.bounce.setTo(1);
   },
 
   update: function(){
+    game.physics.arcade.collide([this.player, this.player2], this.ball);
+
+    if(this.ball.y > this.player.y+20) {
+      game.state.start('main');
+    }
+    if(this.ball.y < this.player2.y-20) {
+      game.state.start('main');
+    }
+
     if (this.left.isDown){
       this.player.body.velocity.x = -300;
     }
@@ -25,6 +57,16 @@ var mainState = {
     }
     else {
       this.player.body.velocity.x = 0;
+    }
+
+    if (this.leftP2.isDown){
+      this.player2.body.velocity.x = -300;
+    }
+    else if (this.rightP2.isDown){
+      this.player2.body.velocity.x = 300;
+    }
+    else {
+      this.player2.body.velocity.x = 0;
     }
   }
 };
